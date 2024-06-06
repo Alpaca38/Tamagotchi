2e2e8df0bd8cc2444af1f0a23fa7ffc54c2db9f4
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-06-06 13:44:27 +0900

    Initial Commit

---
+++ Tamagotchi/SelectViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by 조규연 on 6/6/24.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
